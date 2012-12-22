ddf324e4660299e243c9766572a79e463f003490
Thomas Levine <occurrence@thomaslevine.com>
2012-12-22 17:14:02 -0500

    plots with a concept of error

--- merry.r
+++ merry.r
@@ -11,12 +11,22 @@ merry[is.na(merry[,'TRUE']),'TRUE'] <- 0
 merry$total <- merry[,'TRUE'] + merry[,'FALSE']
 
 merry$p <- merry[,'TRUE'] / merry$total
+merry$se <- sqrt(merry$p * (1-merry$p) / merry$total)
 
-p.merry <- ggplot(merry) + aes(x = year, y = p, size = total) + geom_line() +
+p.merry <- ggplot(merry) + aes(x = year, y = p) + geom_line() +
   geom_vline(xintercept = 1949, color = 'red') +
   scale_x_continuous('Year of birth') +
   scale_y_continuous('Proportion named "Merry"', labels = percent) +
-  scale_size_continuous('Total people') +
+  labs(title = 'When was the name "Merry" popular?\nBased on dead people from the Social Security Death Master File')
+
+p.merry.error <- p.merry +
+  geom_errorbar(aes(ymin = p + qnorm(0.25) * se, ymax = p + qnorm(0.975) * se))
+
+merry$low.estimate <- sapply(merry$p + qnorm(0.05) * merry$se, function(v) {max(0, v)})
+p.merry.low <- ggplot(merry) + aes(x = year, y = low.estimate) + geom_line() +
+  geom_vline(xintercept = 1949, color = 'red') +
+  scale_x_continuous('Year of birth') +
+  scale_y_continuous('Low estimate of proportion of people named "Merry"\n(single-tailed normal 95% confidence interval imagining a superpopulation)', labels = percent) +
   labs(title = 'When was the name "Merry" popular?\nBased on dead people from the Social Security Death Master File')
 
 p.all <- ggplot(merry) + aes(x = year, y = total) + geom_line() +
@@ -27,4 +37,5 @@ p.all <- ggplot(merry) + aes(x = year, y = total) + geom_line() +
 pdf('merry.pdf', width = 11, height = 8.5)
 print(p.all)
 print(p.merry)
+print(p.merry.low)
 dev.off()
